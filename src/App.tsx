54333d7546eec23ad6ce735bda2a1f365178ec46
TaeHan <goldmini95@gmail.com>
2023-08-11 19:47:19 +0900

    setting recoil, react-query, routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 import App from './App.tsx'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
