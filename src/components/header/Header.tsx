90e70eca755a84de9d412801d2624a4a70e94cda
taehankim-dev <goldmini95@gmail.com>
2023-09-05 15:48:55 +0900

    헤더 글씨 클릭 시, 홈으로 이동

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import HeaderSign from './HeaderSign';
 
 import { HeaderContainer } from '@styles/HeaderStyle';
+import { useNavigate } from 'react-router-dom';
 
 const Header : React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
       <HeaderContainer>
         <div></div>
         <div style={{textAlign:'center', cursor:'default'}}>
-          <h1>오늘의 할 일</h1>
+          <h1>
+            <span style={{cursor:"pointer"}} onClick={() => {navigate("/")}}>오늘의 할 일</span>
+          </h1>
         </div>
         <HeaderSign />
       </HeaderContainer>
