f0d66635ef2faa7200b0c2f3c6a2c0a1486407ed
taehankim-dev <goldmini95@gmail.com>
2023-09-05 15:49:18 +0900

    로그인 안했을 시 구분 처리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { PageContainerWithMenu } from '@styles/PageLayout';
 import Header from '@components/header/Header';
-import Menu from '@components/Menu';
+import { useRecoilValue } from 'recoil';
+import { isLoginState } from '@states/UserState';
+import Calendar from './Calendar';
+// import Menu from '@components/Menu';
 
 
 const Home : React.FC = () => {
+  const isLogIn = useRecoilValue(isLoginState)
+
   return (
     <>
       <Header />
       <PageContainerWithMenu>
-        <Menu />
-        home
+        {/* <Menu /> */}
+        {
+          isLogIn 
+          ?
+          <Calendar />
+          :
+          <>Home</>
+        }
       </PageContainerWithMenu>
     </>
     
