ab0a6a0bfb504dad327504af4484daafe3f682a2
TaeHan <goldmini95@gmail.com>
2023-10-10 15:39:43 +0900

    header의 중복되는 loading 값 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,13 @@
 import React, { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { isLoginState } from '@states/UserState';
-import { isLoadingState } from '@states/PopupState';
 import Header from '@components/header/Header';
 import NonMemberHome from './nonMembers/Home';
-import Loading from '@components/loading/Loading';
 import CalendarHome from './members/calendar/CalendarHome';
 import Menu from '@components/menu/Menu';
 
 const Home : React.FC = () => {
   const isLogIn = useRecoilValue(isLoginState);
-  const loading = useRecoilValue(isLoadingState);
-  console.log(loading);
-  
-  useEffect(() => {
-    console.log(loading)
-  }, [loading])
 
   useEffect(() => {
     console.log(isLogIn)
@@ -25,14 +17,11 @@ const Home : React.FC = () => {
     <>
       <Header />
       <Menu />
-      {!loading ? 
-          isLogIn ? 
-          <CalendarHome />
-          :
-          <NonMemberHome />
+      {isLogIn ? 
+        <CalendarHome />
         :
-        <Loading /> 
-      }
+        <NonMemberHome />
+      }        
     </>
     
   )
